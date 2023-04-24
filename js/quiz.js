7b6e8f29abb24b6c1d66d2107fb822016dc3e568
AJosueBN <anthonyjosue.bn@gmail.com>
2023-04-24 22:19:24 +0000

    Created HTML document and started adding variables in JavaScript and organised them to categories

---
+++ js/quiz.js
@@ -0,0 +1,28 @@
+var startQuiz = document.getElementById("startQuiz")
+
+
+
+
+var choices = document.getElementById("choices")
+var message = document.getElementById("message")
+
+var timer = document.getElementById("timer")
+
+var summary = document.getElementById("summary")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+startQuiz.addEventListener("click" , )

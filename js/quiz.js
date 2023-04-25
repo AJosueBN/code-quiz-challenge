203adda9db417aaf335122cc3b0f71dde998946e
AJosueBN <anthonyjosue.bn@gmail.com>
2023-04-25 17:38:30 +0000

    Styled css and added in function for starting quiz

--- js/quiz.js
+++ js/quiz.js
@@ -1,6 +1,9 @@
+// Variables set globally
+
 var startQuiz = document.getElementById("startQuiz")
 
-
+var welcome = document.getElementById("welcome")
+var quiz = document.getElementById("quiz")
 
 
 var choices = document.getElementById("choices")
@@ -8,7 +11,33 @@ var message = document.getElementById("message")
 
 var timer = document.getElementById("timer")
 
-var summary = document.getElementById("summary")
+var outcome = document.getElementById("outcome")
+
+var secondsLeft = 0;
+var score = 0;
+var currentQuestion = 0;
+var countdownTimer;
+
+// This section helps to start the quiz
+function onStartQuiz() {
+startQuiz.classList.add('hide')
+
+// Time given to answer questions
+secondsLeft = 25
+currentQuestion = 0;
+
+countdownTimer = setInterval() 
+   
+    if(secondsLeft > 0) {
+    timer.textContent = secondsLeft;
+   } else {
+    stopQuiz();
+   }
+   secondsLeft--;
+
+
+
+}
 
 
 
@@ -18,11 +47,25 @@ var summary = document.getElementById("summary")
 
 
 
+// This section helps display quiz questions
+function displayQuestion() {
 
 
 
+}
+
+// This section helps user to select answer 
+function selectAnswer() {
 
 
 
+}
 
-startQuiz.addEventListener("click" , )
+// This section helps to stop the quiz completely
+function stopQuiz() {
+
+
+}
+
+startQuiz.addEventListener("click" , onStartQuiz);
+playAgain.addEventListener("click" , onStartQuiz);
